6007d18943d61324a3c40f2c4bc596adcede7168
Samarth Sharma <110784602+bugsum@users.noreply.github.com>
2026-01-16 22:34:25 +0530

    fixing config v2

--- app.config.ts
+++ app.config.ts
@@ -1,95 +1,49 @@
+import { ConfigContext, ExpoConfig } from 'expo/config';
 
-import { ExpoConfig, ConfigContext } from 'expo/config';
-
-export default ({ config }: ConfigContext): ExpoConfig => ({
-  ...config,
-  name: 'Symmetry IQ',
-  description: 'A mobile app focused on assisting users ',
-  slug: 'symmetry-iq',
-  owner: 'symmetry-iq',
-  version: '1.0.0',
-  platforms: ['ios', 'android'],
-  orientation: 'portrait',
-  icon: './assets/images/icon.png',
-  scheme: 'symmetryiq',
-  userInterfaceStyle: 'dark',
-  backgroundColor: '#16141f',
-  newArchEnabled: true,
-  ios: {
-    supportsTablet: true,
-    bundleIdentifier: 'com.bergmanmedia.symmetryiq',
-    googleServicesFile: './GoogleService-Info.plist',
-    infoPlist: {
-      ITSAppUsesNonExemptEncryption: false,
+export default ({ config }: ConfigContext): ExpoConfig => {
+  return {
+    ...config,
+    name: 'Symmetry IQ',
+    description: 'A mobile app focused on assisting users ',
+    slug: 'symmetry-iq',
+    owner: 'symmetry-iq',
+    version: '1.0.0',
+    platforms: ['ios', 'android'],
+    orientation: 'portrait',
+    icon: './assets/images/icon.png',
+    scheme: 'symmetryiq',
+    userInterfaceStyle: 'dark',
+    backgroundColor: '#16141f',
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: 'com.bergmanmedia.symmetryiq',
+      googleServicesFile: './GoogleService-Info.plist',
+      infoPlist: {
+        ITSAppUsesNonExemptEncryption: false,
+      },
     },
-  },
-  android: {
-    edgeToEdgeEnabled: true,
-    predictiveBackGestureEnabled: false,
-    package: 'com.bergmanmedia.symmetryiq',
-    googleServicesFile: './google-services.json',
-  },
-  web: {
-    output: 'static',
-    favicon: './assets/images/favicon.png',
-  },
-  plugins: [
-    'expo-router',
-    [
-      'expo-font',
-      {
-        fonts: [
-          'node_modules/@expo-google-fonts/outfit/400Regular/Outfit_400Regular.ttf',
-          'node_modules/@expo-google-fonts/outfit/500Medium/Outfit_500Medium.ttf',
-          'node_modules/@expo-google-fonts/outfit/600SemiBold/Outfit_600SemiBold.ttf',
-          'node_modules/@expo-google-fonts/outfit/700Bold/Outfit_700Bold.ttf',
-        ],
-      },
-    ],
-    [
-      'expo-splash-screen',
-      {
-        image: './assets/images/icon.png',
-        imageWidth: 200,
-        resizeMode: 'contain',
-        backgroundColor: '#ffffff',
-        dark: {
-          backgroundColor: '#16141f',
-        },
-      },
-    ],
-    [
-      'expo-image-picker',
-      {
-        photosPermission:
-          'The app accesses your photos to analyze your face and generate routines.',
-        cameraPermission:
-          'The app accesses your camera to capture pictures and help you improve your face.',
-      },
-    ],
-    [
-      'expo-build-properties',
-      {
-        ios: {
-          useFrameworks: 'static',
-        },
-      },
-    ],
-    'expo-secure-store',
-    '@react-native-firebase/app',
-    '@react-native-firebase/auth',
-    '@react-native-google-signin/google-signin',
-    './plugins/withMediapipeTasks.js',
-  ],
-  experiments: {
-    typedRoutes: true,
-    reactCompiler: true,
-  },
-  extra: {
-    ...config.extra,
-    router: {},
-    eas: {
-      projectId: '51228dab-19a8-4c7f-ac85-cb808412e5a1',
+    android: {
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false,
+      package: 'com.bergmanmedia.symmetryiq',
+      googleServicesFile: './google-services.json',
     },
-  },
-});
+    web: {
+      output: 'static',
+      favicon: './assets/images/favicon.png',
+    },
+    plugins: [...config.plugins!, './plugins/withMediapipeTasks.js'],
+    experiments: {
+      typedRoutes: true,
+      reactCompiler: true,
+    },
+    extra: {
+      ...config.extra,
+      router: {},
+      eas: {
+        projectId: '51228dab-19a8-4c7f-ac85-cb808412e5a1',
+      },
+    },
+  };
+};
